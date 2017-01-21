c5e4ff342de8b760d6155c8e3fde69696cc3d7db
IRSHAD PC <ichu501@gmail.com>
2017-01-21 14:48:37 +0530

    Add .gitignore file

---
+++ FaqView/UIView+Fold.h
@@ -0,0 +1,13 @@
+//
+//  UIView+Fold.h
+//  FaqView
+//
+//  Created by IRSHAD PC on 21/01/17.
+//  Copyright © 2017 IRSHAD PC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIView (Fold)
+
+@end
---
+++ FaqView/UIView+Fold.m
@@ -0,0 +1,13 @@
+//
+//  UIView+Fold.m
+//  FaqView
+//
+//  Created by IRSHAD PC on 21/01/17.
+//  Copyright © 2017 IRSHAD PC. All rights reserved.
+//
+
+#import "UIView+Fold.h"
+
+@implementation UIView (Fold)
+
+@end
