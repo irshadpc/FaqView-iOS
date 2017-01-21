52448dac61c87a5d8eb0967274e72c158fd7c0a4
IRSHAD PC <ichu501@gmail.com>
2017-01-21 14:51:50 +0530

    Create a category for view folding and expand animation

--- FaqView/UIView+Fold.h
+++ FaqView/UIView+Fold.h
@@ -10,4 +10,10 @@
 
 @interface UIView (Fold)
 
+- (void)foldOpenWithTransparency:(BOOL)withTransparency
+             withCompletionBlock:(void (^)(void))completionBlock;
+
+- (void)foldClosedWithTransparency:(BOOL)withTransparency
+               withCompletionBlock:(void (^)(void))completionBlock;
+
 @end
